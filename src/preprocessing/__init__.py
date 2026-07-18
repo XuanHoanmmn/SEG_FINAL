370d15b707f3b063f719ae576aea583b0881cd5e
XuanHoanmmn <domon26102006@gmail.com>
2026-07-18 11:58:03 +0700

    Add auditable cleanup and weighted query expansion

--- src/preprocessing/__init__.py
+++ src/preprocessing/__init__.py
@@ -15,17 +15,23 @@ from src.preprocessing.pipeline import (
     process_document,
     process_jsonl,
 )
+from src.preprocessing.quality import (
+    DocumentQualityChanges,
+    sanitize_recipe_document,
+)
 
 __all__ = [
     "INDEXED_FIELDS",
     "ProcessedRecipe",
     "ProcessingReport",
+    "DocumentQualityChanges",
     "VietnameseTextProcessor",
     "build_search_forms",
     "iter_processed_jsonl",
     "normalize_text",
     "process_document",
     "process_jsonl",
+    "sanitize_recipe_document",
     "strip_accents",
     "tokenize_basic",
 ]
