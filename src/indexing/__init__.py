17641b9245df56261b10c8e20a4701e13e62115e
XuanHoanmmn <domon26102006@gmail.com>
2026-07-18 08:43:34 +0700

    feat: export positional index

--- src/indexing/__init__.py
+++ src/indexing/__init__.py
@@ -1,2 +1,19 @@
 """Custom inverted-index construction and persistence."""
 
+from src.indexing.inverted_index import (
+    INDEX_CHANNELS,
+    INDEX_FORMAT_VERSION,
+    IndexStatistics,
+    PositionalInvertedIndex,
+    Posting,
+    build_inverted_index,
+)
+
+__all__ = [
+    "INDEX_CHANNELS",
+    "INDEX_FORMAT_VERSION",
+    "IndexStatistics",
+    "PositionalInvertedIndex",
+    "Posting",
+    "build_inverted_index",
+]
