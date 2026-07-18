43826951821b40a29ec6e27a14e5494ed774e6a8
XuanHoanmmn <domon26102006@gmail.com>
2026-07-18 11:58:14 +0700

    Add auditable cleanup and weighted query expansion

--- src/query/__init__.py
+++ src/query/__init__.py
@@ -1,6 +1,12 @@
 """Query parsing and normalization."""
 
 from src.query.filters import SearchFilters
+from src.query.expansion import VietnameseRecipeQueryExpander
 from src.query.processor import ProcessedQuery, QueryProcessor
 
-__all__ = ["ProcessedQuery", "QueryProcessor", "SearchFilters"]
+__all__ = [
+    "ProcessedQuery",
+    "QueryProcessor",
+    "SearchFilters",
+    "VietnameseRecipeQueryExpander",
+]
